b54ff4c4cfeb6df90dad66b8056edcec02e63228
ad0bert <pe.ortner@gmail.com>
2019-03-08 21:39:13 +0100

    create folders out of open api spec

--- parseOpenApi.js
+++ parseOpenApi.js
@@ -1,4 +1,18 @@
 var http = require('https');
+var path = require('path');
+var fs = require('fs');
+
+async function createDirectories(pathname) {
+   const __dirname = path.resolve();
+   pathname = pathname.replace(/^\.*\/|\/?[^\/]+\.[a-z]+|\/$/g, ''); // Remove leading directory markers, and remove ending /file-name.extension
+   await fs.mkdir(path.resolve(__dirname, pathname), { recursive: true }, e => {
+       if (e) {
+           console.error('Failed');
+       } else {
+           console.log('Success');
+       }
+    });
+}  
 
 function get(url) {
   var request = http.get(url, function(response) {
@@ -9,7 +23,20 @@ function get(url) {
     });
     response.on('end', function() {
       let data = JSON.parse(body);
-      console.log(data);
+      for (let path in data.paths) {
+        let toCreate = '/mocks' + path.replace(/{.*}/g,'__');
+	for (let fileName in data.paths[path]) {
+          let toWrite = toCreate + '/' + fileName + '.mock';
+          createDirectories(toWrite);
+          toWrite = __dirname + toWrite;
+	  if (!fs.exists(toWrite)) {
+	    fs.writeFile(toWrite, null, function(err) {
+	      if (err) console.log('err');
+              console.log('create file');
+	    });
+	  }
+	}
+      }
     });
   });
 }
