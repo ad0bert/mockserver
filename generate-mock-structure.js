83f36c35b0c9bf40de22a5c3cc29b90eb7937a88
Peter Ortner <pe.ortner@gmail.com>
2019-03-15 15:14:53 +0100

    Update generate-mock-structure.js
    handle https and local json files

--- generate-mock-structure.js
+++ generate-mock-structure.js
@@ -1,4 +1,5 @@
 const http = require('http');
+const https = require('https');
 const path = require('path');
 const fs = require('fs');
 
@@ -13,30 +14,44 @@ function createDirectories(pathname) {
   });
 }
 
-function get(url) {
-  http.get(url, function(response) {
-    let body = '';
-    response.on('data', function(chunk) {
-      body += chunk;
-    });
-    response.on('end', async function() {
-      let data = JSON.parse(body);
-      for (let path in data.paths) {
-        let toCreate = '/mocks' + data.basePath + path.replace(/{.*}/g,'__');
-        let fileCreate = data.basePath + path.replace(/{.*}/g,'__');
-        for (let fileName in data.paths[path]) {
-          let toWrite = toCreate + '/';
-          await createDirectories(toWrite);
-          toWrite = __dirname + fileCreate + '/' + fileName.toUpperCase() + '.mock';
-          if (!fs.existsSync(toWrite)) {
-            fs.writeFile(toWrite, '', function(err) {
-              if (err) console.log(err);
-            });
-          }
-        }
+function buildDirectoryTree(data) {
+  for (let path in data.paths) {
+    let toCreate = '/mocks' + data.basePath + path.replace(/{.*}/g, '__');
+    let fileCreate = data.basePath + path.replace(/{.*}/g, '__');
+    for (let fileName in data.paths[path]) {
+      let toWrite = toCreate + '/';
+      createDirectories(toWrite);
+      toWrite = __dirname + fileCreate + '/' + fileName.toUpperCase() + '.mock';
+      if (!fs.existsSync(toWrite)) {
+        fs.writeFile(toWrite, '', function (err) {
+          if (err) console.log(err);
+        });
       }
-    });
+    }
+  }
+}
+
+function handleHttpResponse(response) {
+  let body = '';
+  response.on('data', function (chunk) {
+    body += chunk;
   });
+  response.on('end', function () {
+    buildDirectoryTree(JSON.parse(body));
+  });
+}
+
+function get(url) {
+  if (url.startsWith('https')) {
+    https.get(url, handleHttpResponse);
+  } else if (url.startsWith('http')) {
+    http.get(url, handleHttpResponse);
+  } else {
+    const filePath = path.resolve(__dirname, url);
+    if (fs.existsSync(filePath)) {
+      buildDirectoryTree(JSON.parse(fs.readFileSync(filePath).toString()));
+    }
+  }
 }
 
 get(process.argv[2]);
